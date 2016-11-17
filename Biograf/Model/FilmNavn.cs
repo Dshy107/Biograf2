b4837dfefe50ecb7ba348a48e8422ad2d4324d1c
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-11-17 11:24:48 +0100

    Add project files.

---
+++ Biograf/Model/FilmNavn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biograf.Model
+{
+    public class FilmNavn
+    {
+        public string filmNavn { get; set; }
+        public double pris { get; set; }
+        public DateTime releaseDato { get; set; }
+        public string description { get; set; }
+        public bool treDtoD { get; set; }
+        public string sal { get; set; }
+        public int rating { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Titel: " + filmNavn + ", " + "Pris: " + pris + ", " + "I biografen: " + releaseDato + ", " + "Beskrivelse: " + description + ", " + sal + ", " + "Rating: " + rating;
+        }
+
+    }
+}
---
+++ Biograf/ViewModel/FilmViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biograf.ViewModel
+{
+    public class FilmViewModel 
+    {
+        public Model.FilmList Filmliste { get; set; }
+        private Model.FilmNavn selectedFilm;
+
+        public Model.FilmNavn SelectedFilm
+        {
+            get { return selectedFilm; }
+            set { selectedFilm = value; }
+        }
+
+
+        public FilmViewModel()
+        {
+            Filmliste = new Model.FilmList();
+            selectedFilm = new Model.FilmNavn();
+        }
+    }
+}
